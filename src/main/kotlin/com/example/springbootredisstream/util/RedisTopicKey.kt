39b0c41154c1a6b0ee5add954ab8e909c844df71
onenayooW <wittawat_o@tripetch-it.co.th>
2023-03-30 18:50:41 +0700

    feat: spring boot redis stream

--- src/main/kotlin/com/example/springbootredisstream/util/RedisTopicKey.kt
+++ src/main/kotlin/com/example/springbootredisstream/util/RedisTopicKey.kt
@@ -1,9 +1,9 @@
 package com.example.springbootredisstream.util
 
 object RedisTopicKey {
-    const val USER_TOPIC = "user"
-    const val USER_GROUP_1_TOPIC = "user-1"
-    const val PRODUCT_TOPIC = "product"
-    const val PRODUCT_GROUP_1_TOPIC = "product-1"
+    const val USER_TOPIC = "USER"
+    const val USER_GROUP_1_TOPIC = "USER-1"
+    const val PRODUCT_TOPIC = "PRODUCT"
+    const val PRODUCT_GROUP_1_TOPIC = "PRODUCT-1"
 }
 
